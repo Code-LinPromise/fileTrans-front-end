d6fafe905eef7758486052d8ffcf520bc6998560
lin pengtao <326661445@qq.com>
2024-03-05 20:40:44 +0800

    引入taiwind ract-router

--- fileTrans-front-end/src/main.tsx
+++ fileTrans-front-end/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./tailwind.css"
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './router';
+
+const hashRouter=createHashRouter(router)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={hashRouter} />
   </React.StrictMode>,
 )
---
+++ fileTrans-front-end/src/router/index.tsx
@@ -0,0 +1,11 @@
+import Home from "../pages/Home"
+
+export const router=[
+    {
+        path: "/",
+        element: <Home />,
+        children: [
+         
+        ],
+    },
+]
